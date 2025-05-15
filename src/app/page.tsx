c2ba77098d9da89eade2c627a65e43879249669e
lsc-ham <hmarandang@lakeshore.edu.ph>
2025-05-15 11:34:31 +0800

    logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import { supabase } from '@/lib/supabaseClient';
+import Image from 'next/image';
 import {
   BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer,
   LabelList,
@@ -49,34 +50,43 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="flex flex-col gap-8 p-4 items-center w-full h-screen">
-      <h1 className="flex text-2xl font-bold">Enrollment Summary</h1>
+    <main className="flex flex-col items-center w-full h-screen">
+      <section className='flex flex-col items-center w-full p-4'>
+        <Image
+          src="/img/lsc-logo.png"
+          alt="LSC Logo"
+          width={150}
+          height={150}
+        />
+        <h1 className="flex text-2xl font-bold ">Enrollment Summary</h1>
+      </section>
+      <section className='flex flex-col w-full items-center h-full'>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <ResponsiveContainer width="100%" height="100%">
+            <BarChart
+              data={enrollmentData}
+              margin={{ top: 20, right: 30, left: 20, bottom: 120 }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis
+                dataKey="program_name"
+                angle={-30}
+                textAnchor="end"
+                interval={0}
+                height={100}
+              />
+              <YAxis />
+              <Tooltip />
+              <Bar dataKey="num_of_enrolled" fill="#356854">
+                <LabelList dataKey="num_of_enrolled" position="top" style={{ fontSize: '24px', fontWeight: 'semibold', fill: '#333' }} />
+              </Bar>
+            </BarChart>
+          </ResponsiveContainer>
 
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <ResponsiveContainer width="100%" height="100%">
-          <BarChart
-            data={enrollmentData}
-            margin={{ top: 20, right: 30, left: 20, bottom: 60 }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis
-              dataKey="program_name"
-              angle={-45}
-              textAnchor="end"
-              interval={0}
-              height={100}
-            />
-            <YAxis />
-            <Tooltip />
-            <Bar dataKey="num_of_enrolled" fill="#3b82f6">
-              <LabelList dataKey="num_of_enrolled" position="top" style={{ fontSize: '24px', fontWeight: 'semibold', fill: '#333' }} />
-            </Bar>
-          </BarChart>
-        </ResponsiveContainer>
-
-      )}
+        )}
+      </section>
     </main>
   );
 }
